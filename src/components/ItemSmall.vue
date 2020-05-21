76f1dd8e592b61860c494ce2a3e8a23f4a2bb561
shlande <shlandeme@gmail.com>
2020-05-21 12:21:00 +0800

    手动css实现hover

--- src/components/ItemSmall.vue
+++ src/components/ItemSmall.vue
@@ -1,31 +1,28 @@
 <template>
   <v-hover v-slot:default="{ hover }">
   <v-card
-      class="d-flex align-end"
+      class=""
       img="/cover-normal.jpg"
       dark
       max-width="370"
       height="200"
       style="overflow:hidden;"
   >
-
-    <v-row>
-      <div style="height: 50px;width: 370px" class="white black--text col-12"><span>ssss</span></div>
-      <v-expand-transition>
-        <div
-            v-if="hover"
-            class="col-12 d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text col-12"
-            style="height: 150px;width: 370px"
-        >
-          $14.99
-        </div>
-      </v-expand-transition>
-    </v-row>
+      <div
+          :class="(!hover?'hover_before':'hover_after')+' d-flex transition-in-fast-out orange darken-2 v-card--reveal display-3 white--text'"
+          style='height: 100%;width: 100%;position:relative;top:150px'
+      >
+        <div style="height: 60px;width: 100%" class="white"></div>
+      </div>
   </v-card>
   </v-hover>
 </template>
 <style scoped>
-  .hover {
-
+  .hover_before {
+    transition-duration: 0.1s;
+  }
+  .hover_after {
+    transform: translateY(-150px);
+    transition-duration: 0.1s;
   }
 </style>
