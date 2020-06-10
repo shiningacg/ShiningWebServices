1fdf03797884d7d816685ee5d00cc95148509aa3
shlande <shlandeme@gmail.com>
2020-06-10 23:23:26 +0800

    添加了介绍修改；添加了密码修改；完成了菜单切换效果

--- src/views/Dashboard/User.vue
+++ src/views/Dashboard/User.vue
@@ -25,8 +25,9 @@
               <v-list
                   nav
               >
-                <v-list-item-group color="primary">
+                <v-list-item-group color="primary" v-model="active">
                   <v-list-item
+                      :value="0"
                   >
                     <v-list-item-icon>
                       <v-icon>mdi-account</v-icon>
@@ -36,22 +37,15 @@
                     </v-list-item-content>
                   </v-list-item>
                   <v-list-item
+                      :value="1"
                   >
                     <v-list-item-icon>
                       <v-icon>mdi-account</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                      <v-list-item-title>用户信息</v-list-item-title>
+                      <v-list-item-title>更改密码</v-list-item-title>
                     </v-list-item-content>
-                  </v-list-item><v-list-item
-                >
-                  <v-list-item-icon>
-                    <v-icon>mdi-account</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>用户信息</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
+                  </v-list-item>
                 </v-list-item-group>
               </v-list>
         </v-card>
@@ -59,11 +53,13 @@
       <!--信息展示-->
       <v-col cols="9" class="pa-0">
         <v-card height="100%" width="100%" flat class="pa-4">
-          <!--头像展示-->
-          <div class="d-flex justify-center">
-            <v-avatar
-              size="128"
-            >
+          <!--用户信息-->
+          <div v-if="active==0">
+            <!--头像展示-->
+            <div class="d-flex justify-center">
+              <v-avatar
+                  size="128"
+              >
                 <v-img
                     :src="avatar"
                 >
@@ -72,42 +68,81 @@
                       修改头像
                     </div>
                   </v-sheet>
-              </v-img>
-            </v-avatar>
+                </v-img>
+              </v-avatar>
+            </div>
+            <div class="d-flex justify-center">
+              <v-card width="600" flat class="pt-4">
+                <v-row>
+                  <v-col cols="12" class="pa-0">
+                    <v-textarea
+                        filled
+                        name="description"
+                        rounded
+                        label="简介"
+                        rows="3"
+                        :placeholder="description"
+                        :value="info.description"
+                    ></v-textarea>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center">
+                    <div style="width: 80px" class="text-end pr-4">用户名：</div>
+                    <v-text-field
+                        append-outer-icon="mdi-account"
+                        :placeholder="name"
+                        :value="info.name"
+                    >
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center">
+                    <div style="width: 80px" class="text-end pr-4" >邮箱：</div>
+                    <v-text-field
+                        append-outer-icon="mdi-account"
+                        :placeholder="email"
+                        :value="info.email"
+                    >
+
+                    </v-text-field>
+                  </v-col>
+                </v-row>
+                <div class="d-flex justify-end pt-8 pb-4" >
+                  <v-btn color="primary">save</v-btn>
+                </div>
+              </v-card>
+            </div>
           </div>
-          <div class="d-flex justify-center">
-            <v-card width="600" flat class="pt-4">
-              <v-row>
-                <v-col cols="12" class="d-flex align-center">
-                  <div style="width: 80px" class="text-end pr-4">用户名：</div>
-                  <v-text-field
-                      append-outer-icon="mdi-account"
-                      placeholder="shlande"
-                  >
-
-                  </v-text-field>
-                </v-col>
-                <v-col cols="12" class="d-flex align-center">
-                  <div style="width: 80px" class="text-end pr-4">密码：</div>
-                  <v-text-field
-                      append-outer-icon="mdi-account"
-                      placeholder="shlande"
-                  >
-
-                  </v-text-field>
-                </v-col>
-                <v-col cols="12" class="d-flex align-center">
-                  <div style="width: 80px" class="text-end pr-4" >邮箱：</div>
-                  <v-text-field
-                      append-outer-icon="mdi-account"
-                      placeholder="shlande"
-                  >
-
-                  </v-text-field>
-                </v-col>
-              </v-row>
-              <div class="d-flex justify-end pt-8 pb-4" >
-                <v-btn color="primary">save</v-btn>
+          <!--密码修改-->
+          <div class="d-flex justify-center" v-if="active==1">
+            <v-card width="600" class="pt-6" flat>
+              <div class="d-flex align-center">
+                <div style="width: 80px;" class="mr-2">当前密码：</div>
+                <v-text-field
+                    :type="pwd.show1?'text':'password'"
+                    :value="pwd.currentPWD"
+                    :append-icon="pwd.show1?'mdi-eye-off':'mdi-eye'"
+                    @click:append="pwd.show1=!pwd.show1"
+                ></v-text-field>
+              </div>
+              <div class="d-flex align-center">
+                <div style="width: 80px;" class="mr-2">重置密码：</div>
+                <v-text-field
+                    :type="pwd.show2?'text':'password'"
+                    :value="pwd.newPWD"
+                    :append-icon="pwd.show2?'mdi-eye-off':'mdi-eye'"
+                    @click:append="pwd.show2=!pwd.show2"
+                ></v-text-field>
+              </div>
+              <div class="d-flex align-center">
+                <div style="width: 80px;" class="mr-2">确认密码：</div>
+                <v-text-field
+                    :type="pwd.show3?'text':'password'"
+                    :value="pwd.confirmPWD"
+                    :append-icon="pwd.show3?'mdi-eye-off':'mdi-eye'"
+                    @click:append="pwd.show3=!pwd.show3"
+                ></v-text-field>
+              </div>
+              <div class="d-flex justify-end pt-8 pr-4">
+                <v-btn color="primary" elevation="0">确认</v-btn>
               </div>
             </v-card>
           </div>
@@ -117,7 +152,7 @@
 </template>
 <script>
   import is_dev_env from "@/utils/is_dev_env";
-  import mock from "@/mock/user.json"
+  import mock from "@/mock/user-full.json"
   export default {
     name: "User",
     props: { input: {} },
@@ -130,10 +165,24 @@
     },
     data() {
       return {
+        active: 0,
         name: "",
         avatar: "",
+        email:"",
         description: "",
-        change: {}
+        info: {
+          description: "",
+          name: "",
+          email: "",
+        },
+        pwd : {
+          currentPWD:"",
+          newPWD:"",
+          confirmPWD:"",
+          show1: false,
+          show2: false,
+          show3: false,
+        }
       }
     },
     methods: {
@@ -141,7 +190,8 @@
         this.name = user.name
         this.avatar = user.avatar
         this.description = user.description
-      }
+        this.email = user.email
+      },
     }
   }
 </script>
